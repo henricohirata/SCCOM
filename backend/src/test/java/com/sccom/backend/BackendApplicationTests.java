08ff4ffd80c81a2ad931a9625988f54e2b6650a0
Henrico Hirata <henricobhirata@gmail.com>
2025-12-18 18:54:51 -0300

    --setup: backend, frontend, e docker configurados

---
+++ backend/src/test/java/com/sccom/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sccom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
